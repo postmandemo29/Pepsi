62fdc0569836771fa6e6798576a6586d4d4be5f4
Buchii <Buchii@abc.com>
2024-05-31 23:09:04 +0000

    I commit new DEv file

---
+++ Pepsi1ListUser.js
@@ -0,0 +1,39 @@
+const platformClient = require("purecloud-platform-client-v2");
+
+const client = platformClient.ApiClient.instance;
+client.setEnvironment(platformClient.PureCloudRegionHosts.us_east_1); // Genesys Cloud region
+
+// Manually set auth token or use loginImplicitGrant(...) or loginClientCredentialsGrant(...) or loginPKCEGrant(...)
+//client.setAccessToken("your_access_token");
+const clientId = "d6e3ad13-574a-49d9-9d88-1f2c84a0db84";
+const clientSecret = "AmiDoqO-k5BtAWFNXmYGDn685omH_YOvI6gofNBvF4c";
+client.loginClientCredentialsGrant(clientId, clientSecret)
+    .then(() => {    
+let apiInstance = new platformClient.UsersApi();
+
+let opts = { 
+  "pageSize": 25, // Number | Page size
+  "pageNumber": 1, // Number | Page number
+  //"id": ["id_example"], // [String] | A list of user IDs to fetch by bulk
+  //"jabberId": ["jabberId_example"], // [String] | A list of jabberIds to fetch by bulk (cannot be used with the id parameter)
+  "sortOrder": "ASC", // String | Ascending or descending sort order
+  //"expand": ["expand_example"], // [String] | Which fields, if any, to expand
+  //"integrationPresenceSource": "integrationPresenceSource_example", // String | Gets an integration presence for users instead of their defaults. This parameter will only be used when presence is provided as an expand. When using this parameter the maximum number of users that can be returned is 100.
+  "state": "active" // String | Only list users of this state
+};
+
+// Get the list of available users.
+apiInstance.getUsers(opts)
+  .then((data) => {
+    console.log(`getUsers success! data: ${JSON.stringify(data, null, 2)}`);
+  })
+  .catch((err) => {
+    console.log("There was a failure calling getUsers");
+    console.error(err);
+  });
+})
+.catch((err) => {
+    console.log("There was a failure calling loginClientCredentialsGrant");
+    console.error(err);
+});
+
